0e9a268094e146a08621950367d9e6869ac28a02
elaltas <elaltas@gmail.com>
2021-01-05 01:40:47 +0100

    docu

--- src/main/java/com/delalama/SpringKarate/Controller/GreetingController.java
+++ src/main/java/com/delalama/SpringKarate/Controller/GreetingController.java
@@ -1,10 +1,10 @@
 package com.delalama.SpringKarate.Controller;
 
+import com.delalama.SpringKarate.Modelç.Name;
+import com.google.gson.Gson;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,6 +16,18 @@ public class GreetingController {
     public Map<String, String> getGreeting() {
         HashMap<String, String> map = new HashMap<>();
         map.put("greeting", "hello Sir");
+        return map;
+    }
+
+    @PostMapping(path = "greeting", produces= MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, String> postGreeting(
+            @RequestBody String name ) {
+        Gson gson = new Gson();
+        Name name1 = gson.fromJson(name, Name.class);
+
+        HashMap<String, String> map = new HashMap<>();
+        map.put("greeting", "hello " + name1.getName() );
+
         return map;
     }
 }
--- src/test/java/karate/greetingController/greeting.feature
+++ src/test/java/karate/greetingController/greeting.feature
@@ -3,9 +3,26 @@ Feature: Greeting generator
   Background:
     * url baseUrl
 
-  Scenario: Fetch greeting
+  Scenario: Get greeting
 
     Given path 'api/greeting'
     When method GET
     Then status 200
     And match $ == {greeting:'#notnull'}
+    And match $ != {greeting:'bye bye'}
+
+  Scenario Outline: Post greeting
+
+    Given path 'api/greeting'
+    And request {name:'<name>'}
+    When method POST
+    Then status 200
+    And match $ == {'greeting': 'hello <name>'}
+
+    Examples:
+      | name  |
+      | Tim   |
+      | Liz   |
+      | Selma |
+      | Ted   |
+      | Luise |
